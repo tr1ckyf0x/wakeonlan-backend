dbf373aab06a037ecabfe52ccb3599fa49f6bf26
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2021-08-31 22:12:53 +0300

    Configured Heroku DB

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -27,14 +27,30 @@ public func configure(_ app: Application) throws {
         )
     )
     app.middleware.use(cors)
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    
+    switch app.environment {
+    // Used for Heroku
+    case .production:
+        guard let databaseURL = Environment.get("DATABASE_URL"),
+              var postgresConfig = PostgresConfiguration(url: databaseURL)
+        else {
+            break
+        }
+        
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+        
+    default:
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
+    }
 
     app.migrations.add(CreateFeedbackRecords())
 
