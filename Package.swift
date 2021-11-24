0ed9badb12d934fffb03c3ac743b28c441bda781
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2021-11-24 23:00:33 +0300

    Refactored using async/await

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "wakeonlan-backend",
     platforms: [
-        .macOS(.v11)
+        .macOS(.v12)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
--- Sources/App/Controllers/SupportController.swift
+++ Sources/App/Controllers/SupportController.swift
@@ -14,33 +14,28 @@ struct SupportController: RouteCollection {
         feedbackGroup.post(use: create)
     }
 
-    func create(req: Request) throws -> EventLoopFuture<FeedbackRecord> {
+    func create(req: Request) async throws -> FeedbackRecord {
         guard let captchaToken = req.headers.first(name: "ReCaptchaToken")
         else {
             throw Abort(.forbidden)
         }
 
-        return req.client.post("https://www.google.com/recaptcha/api/siteverify") { (clientRequest: inout ClientRequest) -> Void in
+        async let verificationResponse = req.client.post("https://www.google.com/recaptcha/api/siteverify", beforeSend: { (clientRequest: inout ClientRequest) -> Void in
             let secret = Environment.get("AWAKE_CAPTCHA_KEY") ?? ""
             try clientRequest.content.encode([
                 "secret": secret,
                 "response": captchaToken
             ], as: .urlEncodedForm)
+        }).content.decode(SiteverifyResponse.self)
+
+        guard try await verificationResponse.isValid else {
+            throw Abort(.forbidden)
         }
-        .flatMapThrowing({ (clientResponse: ClientResponse) -> SiteverifyResponse in
-            let verificationResponse = try clientResponse.content.decode(SiteverifyResponse.self)
-            return verificationResponse
-        })
-        .guard({ (verificationResponse: SiteverifyResponse) -> Bool in
-            verificationResponse.isValid
-        }, else: Abort(.forbidden))
-        .flatMapThrowing({ (_) -> FeedbackRecord in
-            try req.content.decode(FeedbackRecord.self)
-        })
-        .flatMap { (feedbackRecord: FeedbackRecord) -> EventLoopFuture<FeedbackRecord> in
-            return feedbackRecord
-                .save(on: req.db)
-                .map { feedbackRecord }
-        }
+
+        let feedbackRecord = try req.content.decode(FeedbackRecord.self)
+
+        try await feedbackRecord.save(on: req.db)
+
+        return feedbackRecord
     }
 }
